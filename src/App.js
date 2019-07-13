b653432cfc3d4a94827a7df83e2e000297a1b54c
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-07-13 11:17:20 +0200

    Added ninjalist map function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
-
+  state = {
+    ninjas: [
+      { name: 'ryu', age: 25, belt: 'black', id: 1 },
+      { name: 'yoshi', age: 30, belt: 'green', id: 2 },
+      { name: 'crystal', age: 35, belt: 'pink', id: 3 }
+    ]
+  }
 
   render() {
     return (
       <div className="App" >
         <h1> My first React app!</h1>
         <p>Welcome :)</p>
-        <Ninjas name='ryu' age='25' belt='black' />
-        <Ninjas name='yoshi' age='30' belt='green' />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>)
   }
 }
