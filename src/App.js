04bb6204de18cd9722e4bddee244f3e1dce9cc60
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-07-13 12:20:26 +0200

    Added input form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'ryu', age: 25, belt: 'black', id: 1 },
-      { name: 'yoshi', age: 30, belt: 'green', id: 2 },
-      { name: 'crystal', age: 35, belt: 'pink', id: 3 }
+      { name: 'ryu', age: 20, belt: 'black', id: 1 },
+      { name: 'yoshi', age: 25, belt: 'green', id: 2 },
+      { name: 'crystal', age: 30, belt: 'pink', id: 3 }
     ]
   }
 
@@ -16,6 +17,7 @@ class App extends Component {
         <h1> My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja />
       </div>)
   }
 }
