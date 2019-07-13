f695ad706de3a49c1ac90fa9d1e33216bf237201
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-07-13 11:03:49 +0200

    Added Ninjas.js component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first react app</h1>
-      <p>Welcome :)</p>
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App" >
+        <h1> My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas name='ryu' age='25' belt='black' />
+        <Ninjas name='yoshi' age='30' belt='green' />
+      </div>)
+  }
 }
 
 export default App;
